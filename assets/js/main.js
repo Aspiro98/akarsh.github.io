f1a6e5542cd913fe2a73bb4612a109f87fc1a703
Aspiro98 <162844717+Aspiro98@users.noreply.github.com>
2025-03-19 15:31:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ const sendEmail = (e) =>{
    */
 
    // serviceID - templateID - #form - publicKey
-   emailjs.sendForm('service_fz3xvgd','template_5otpcps','#contact-form','fn_XBhe_sNPQOlgTV')
+   emailjs.sendForm('serviceID','templateID','#contact-form','publicKey')
 
    .then(() =>{
       // Show sent message
